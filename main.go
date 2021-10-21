53841d25aacb2048e29343ca7715e061d74d48bb
elvin_dwi_hendrawan <elvin.dwi@ist.id>
2021-10-21 23:26:21 +0700

    create connection to DB

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var db *gorm.DB
+var err error
+
+func main() {
+	// Please define your username and password for MySQL.
+	db, err = gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/belajar-golang?charset=utf8&parseTime=True")
+	// NOTE: See we’re using = to assign the global var
+	// instead of := which would assign it only in this function
+
+	if err != nil {
+		log.Println("Connection Failed to Open")
+	} else {
+		log.Println("Connection Established")
+	}
 }
