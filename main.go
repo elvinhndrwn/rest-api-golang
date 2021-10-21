35ff3c28a12d7309cfc8aa885f9ac4f8d4bc4796
elvin_dwi_hendrawan <elvin.dwi@ist.id>
2021-10-22 00:06:13 +0700

    add sample endpoint & method POST

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -37,4 +42,48 @@ func main() {
 		log.Println("Connection Established")
 	}
 	db.AutoMigrate(&Product{})
+
+	handleRequests()
+}
+
+func handleRequests() {
+	log.Println("Starting development server at http://127.0.0.1:8080/")
+	log.Println("Quit the server with CONTROL-C.")
+	// creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/api/products", createProduct).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
+}
+
+func createProduct(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Create Product")
+
+	// Tangkap payload body dari request, _ artinya tidak perlu menangkap errornya
+	payloads, _ := ioutil.ReadAll(r.Body)
+
+	var product Product
+	json.Unmarshal(payloads, &product)
+
+	// Simpan ke DB
+	db.Create(&product)
+
+	// Set Response
+	res := Result{Code: 200, Data: product, Message: "Success"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to HomePage!")
+	fmt.Println("Endpoint Hit: HomePage")
 }
